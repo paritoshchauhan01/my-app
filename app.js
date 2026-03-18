b46ff5fdaa67b8176c60636ed4f61829715f84d0
paritoshchauhan01 <161223889+paritoshchauhan01@users.noreply.github.com>
2026-03-18 22:09:45 +0530

    add app files, ignore node_modules

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello DevOps!');
+});
+
+app.listen(3000, () => {
+    console.log('App running on http://localhost:3000');
+});
